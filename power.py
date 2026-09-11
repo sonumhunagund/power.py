b70e7e862d5dab9c80957cbf31564828e37623b2
sonumhunagund <sonumhunagund@gmail.com>
2026-09-11 09:53:18 +0530

    saving the file to the github

---
+++ power.py
@@ -0,0 +1,9 @@
+base=float(input("enter the base:"));
+exponent=int(input("enter the exponent:"));
+if(exponent<0):
+    result=1/(base**(exponent))
+else:
+    result=(base**exponent)
+print=("result:",result)    
+        
+ 
